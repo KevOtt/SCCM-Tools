f6d153a16d3caa0e831863676849eaff52b5d096
Kevin Ott <36894453+KevOtt@users.noreply.github.com>
2019-10-02 19:24:53 -0500

    Added Tools

---
+++ Distribution Tools/Redistribute All Failed Packages on Single DP.ps1
@@ -0,0 +1,64 @@
+﻿# Copyright (c) 2019 Kevin Ott
+# Licensed under the MIT License
+# See the LICENSE file in the project root for more information.
+
+
+<# 
+.SYNOPSIS
+	Redistributes all failed packages on a single DP.
+.DESCRIPTION
+    Redistributes any packages for the specified DP which are in state 3,
+    which is failed content distribution.
+.EXAMPLE
+    & '.\Redistribute Failed Packages on Single DP.ps1' -SiteCode EX1 -SiteServer server1.example.com -DP Server2
+.PARAMETER SiteCode
+    Three letter SiteCode for the targeted site.
+.PARAMETER SiteServer
+    FQDN of the primary site server of the targeted site.
+.PARAMETER DP
+    Short name of the DP targeted for redistribution
+.PARAMETER WarningThreshhold
+    Number of packages at which the user will be prompted to redistribute, default 50.
+.NOTES
+    Filename: Redistribute All Failed Packages on Single DP.ps1
+    Version: 
+    Date: 9/20/2019
+    Author: Kevin Ott
+.LINK
+#> 
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$SiteCode,
+    [Parameter(Mandatory=$true)]
+    [string]$SiteServer,
+    [Parameter(Mandatory=$true)]
+    [string]$DP,
+    [int]$WarningThreshhold = 50
+    )
+
+# Get all failed packages for specified DP
+$PackageList = Get-WmiObject -ComputerName $SiteServer -Namespace "root/SMS/Site_$SiteCode" `
+-Class SMS_packagestatusdistpointssummarizer -Filter ('State=3 AND ServerNALPath LIKE ' + ("'%" + $DP + "%'"))
+
+if($Null -eq $PackageList){
+    Throw "No package information returned for $DP"
+    }
+elseif($PackageList.PackageID.Count -le 0){
+    Write-Warning "No failed packaged returned for $DP" -WarningAction Continue
+    exit 0
+    }
+
+# Check packaged numbers, prompt user with package numbers
+if($PackageList.Count -ge $WarningThreshhold){
+    Write-Warning ($PackageList.Count.ToString() + ' packages will be redistributed, continue?') -WarningAction Inquire 
+    }
+
+# Redistribute each package.
+$PackageList | ForEach-Object {
+    Write-Output $_.PackageID
+    $pkgid = $_.PackageID
+    $pkg = Get-WmiObject -ComputerName $SiteServer -Namespace "root/SMS/Site_$SiteCode" `
+    -Class SMS_DistributionPoint -Filter ("PackageID='$pkgid' AND ServerNALPath LIKE " + ("'%" + $DP + "%'"))
+    $pkg.RefreshNow = $true
+    $pkg.Put()
+    }
---
+++ ReportingTools/Generate DP Config Report.ps1
@@ -0,0 +1,136 @@
+﻿# Copyright (c) 2019 Kevin Ott
+# Licensed under the MIT License
+# See the LICENSE file in the project root for more information.
+
+
+<# 
+.SYNOPSIS
+    Creates a report of configurations for all Distribution Points for a given site.
+.DESCRIPTION
+    Pulls a report of various configurations for all Distribution Points on a given
+    site. Can be piped to a file or to an appropriate output format.
+.EXAMPLE
+    & '.\Generate DP Config Report.ps1' -Site 'EX1' -SiteServer Server1.example.com
+.EXAMPLE
+    & '.\Generate DP Config Report.ps1' -Site 'EX1' -SiteServer Server1.example.com | Export-CSV C:\DistributionPoints.csv
+.PARAMETER Site
+    The three letter site code of the site which you are generating seed data for.
+    You will be prompted for one if not provided.  
+.NOTES
+    Filename: Generate DP Config Report.ps1
+    Version: 1.0
+    Date: 10/2/2019
+    Author: Kevin Ott
+#> 
+
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$SiteCode,
+    [Parameter(Mandatory=$true)]
+    [string]$SiteServer
+)
+
+
+
+# Create new custom object with needed properties
+Function Create-DPObject{
+    New-Object -TypeName PSObject -Property ([ordered]@{
+        'ServerName'='';`
+        'Version'='';`
+        'Description'='';`
+        'DPType'='';`
+        'PXEEnabled'='';`
+        'PXEDelay'='';`
+        'UnknownSupport'='';`
+        'MultiCast'='';`
+        'SSLState'='';`
+        'DPGroupCount'='';`
+        'BoundaryGroups'='';`
+        'OS'='';`
+        'IP'='';`
+        'ADSite'='';`
+        'PackagesAssigned'='';`
+        'PackagesInProgress'='';`
+        'PackagesFailed'=''})
+}
+
+# Get Data
+TRY{
+    $DPInfo = Get-WmiObject -ComputerName $SiteServer -Namespace "root/sms/site_$SiteCode" -Class SMS_DistributionPointInfo -ErrorAction Stop -ErrorVariable e | Select-Object *
+    $DPStatus = Get-WmiObject -ComputerName $SiteServer -Namespace "root/sms/site_$SiteCode" -Class SMS_DPStatusInfo -ErrorAction Stop -ErrorVariable e
+    $BoundaryGroups = Get-WmiObject -ComputerName $SiteServer -Namespace "root/sms/site_$SiteCode" -Class SMS_BoundaryGroup -ErrorAction Stop -ErrorVariable e
+    $BoundarySiteSystems = Get-WmiObject -ComputerName $SiteServer -Namespace "root/sms/site_$SiteCode" -Class SMS_BoundaryGroupSiteSystems -ErrorAction Stop -ErrorVariable e
+    }
+CATCH{
+    throw "Unable to retrieve some or all DP data, exception: `n`n $e"
+    }
+
+if(@($DPInfo).Count -le 0){
+    Throw "No DP data returned from site $SiteCode"
+    }
+
+# Pull the data we want
+$DPList= @()
+$DPInfo | ForEach-Object{
+    $p = $_
+    $dp = Create-DPObject
+    
+    $dp.ServerName = $p.ServerName
+    $dp.Version = $p.Properties.Item('Version').Value
+    $dp.Description = $p.Description
+
+
+    if($p.Properties.Item('ispulldp').Value -eq $true){
+        $dp.DPType = 'PullDP'
+        }
+    else{$dp.DPType = 'StandardDP'}
+
+    $dp.PXEEnabled = $p.Properties.Item('ispxe').Value.ToString()
+    $dp.PXEDelay = $p.ResponseDelay.ToString()
+    $dp.UnknownSupport = $p.SupportUnknownMachines.ToString()
+    $dp.MultiCast = $p.Properties.Item('ismulticast').Value.ToString()
+    
+    if($p.Communication -eq 0){
+        $dp.SSLState = 'Http'
+        }
+    else{$dp.SSLState = 'Https'}
+
+    $dp.DPGroupCount = $p.Properties.Item('GroupCount').Value.ToString()
+    
+    # Boundary Groups
+    $groupNames = @()
+    $bids = @($BoundarySiteSystems | Where-Object {$_.ServerNALPath -eq $p.NALPath} | Select-Object -ExpandProperty GroupID)
+    foreach($bid in $bids){
+        $groupNames += (($BoundaryGroups | Where-Object {$_.GroupID -eq $bid} | Select-Object -ExpandProperty Name).ToString())
+        }
+    $dp.BoundaryGroups = $groupNames -join ', '
+
+    # Package Status
+    $s = $DPStatus | Where-Object {$_.NALPath -eq $p.NALPath} 
+    if($null -ne $s){
+        $dp.PackagesAssigned = $s.NumberInstalled
+        $dp.PackagesFailed = $s.NumberErrors
+        $dp.PackagesInProgress = $s.NumberInProgress
+        }
+    else{$dp.PackagesAssigned = $dp.packagesFailed = $dp.PackagesInProgress = 'Unknown'}
+
+
+    # Get client system details
+    $r = Get-WmiObject -ComputerName $SiteServer -Namespace "root/sms/site_$SiteCode" -Query ('SELECT * FROM SMS_R_System WHERE Name = ' + '"' + $p.Name.Split('.')[0] + '"') -ErrorAction SilentlyContinue
+
+    if($null -eq $r -or @($r).Count -gt 1){
+        $dp.IP = $dp.ADSite = $dp.OS = 'Unknown'
+        }
+    else{
+        $dp.IP = $r.IPAddresses -join ';'
+        $dp.ADSite = $r.ADSiteName
+        $dp.OS = $r.OperatingSystemNameandVersion
+        }
+
+
+
+    $DPList += $dp
+    }
+
+
+return $DPList
